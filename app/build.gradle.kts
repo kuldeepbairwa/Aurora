2825782b45ffcc4034700156fc224559db46dadc
Kuldeep Bairwa <kuldeepbairwa8@gmail.com>
2025-02-26 23:56:41 +0530

    ktor core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.hilt.compose.navigation)
 
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.core)
     implementation(libs.ktor.websockets)
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
