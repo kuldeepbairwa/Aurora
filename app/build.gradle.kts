3970525d48c5f762828b2bfed3b81c5b43425c72
Kuldeep Bairwa <kuldeepbairwa8@gmail.com>
2025-02-27 00:11:46 +0530

    added ktor-serializer dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.json.serialization)
     implementation(libs.ktor.websockets)
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
