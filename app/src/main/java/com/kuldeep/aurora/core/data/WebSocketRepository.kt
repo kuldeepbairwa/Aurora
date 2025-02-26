6f61fe21742db5b71a5e67a873c62b59e9377d03
Kuldeep Bairwa <kuldeepbairwa8@gmail.com>
2025-02-27 01:13:01 +0530

    websocket receive+send method impl

--- app/src/main/java/com/kuldeep/aurora/core/data/WebSocketRepository.kt
+++ app/src/main/java/com/kuldeep/aurora/core/data/WebSocketRepository.kt
@@ -1,5 +1,11 @@
 package com.kuldeep.aurora.core.data
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.serialization.Serializable
+
 interface WebSocketRepository {
-    suspend fun connectToWebsocket()
+
+    suspend fun sendToWebSocket(message: Serializable)
+
+    suspend fun receiveFromWebSocket(): Flow<Serializable>
 }
--- app/src/main/java/com/kuldeep/aurora/core/domain/WebSocketRepoImpl.kt
+++ app/src/main/java/com/kuldeep/aurora/core/domain/WebSocketRepoImpl.kt
@@ -2,13 +2,44 @@ package com.kuldeep.aurora.core.domain
 
 import com.kuldeep.aurora.core.data.WebSocketRepository
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.websocket.receiveDeserialized
+import io.ktor.client.plugins.websocket.sendSerialized
+import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.Serializable
 import javax.inject.Inject
 
 class WebSocketRepoImpl @Inject constructor(
     private val client: HttpClient
 ) : WebSocketRepository {
 
-    override suspend fun connectToWebsocket() {
 
+    override suspend fun sendToWebSocket(message: Serializable) {
+        client
+            .webSocket(
+                method = HttpMethod.Get,
+                host = BASE_URL,
+            ) { sendSerialized(message) }
+    }
+
+    override suspend fun receiveFromWebSocket(): Flow<Serializable> {
+        return flow {
+            client
+                .webSocket(
+                    method = HttpMethod.Get,
+                    host = BASE_URL,
+                ) {
+                    val message = receiveDeserialized<Serializable>()
+                    emit(message)
+                }
+        }
+    }
+
+    companion object {
+        const val API_KEY =
+            "kZLNlHLWqcW529bcmszsmJ06GD8C2WRQ45bhZNk7" //always store your api keys securely
+        const val BASE_URL = "wss://s14179.blr1.piesocket.com/v3/1?api_key=$API_KEY&notify_self=1"
     }
 }
