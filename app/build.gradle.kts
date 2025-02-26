53e9d9fbc9488569e79993a7736a692433b848b2
Kuldeep Bairwa <kuldeepbairwa8@gmail.com>
2025-02-27 00:02:05 +0530

    added ktor cio dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
 
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
     implementation(libs.ktor.websockets)
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
